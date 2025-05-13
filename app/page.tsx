83772192baec72e2879fe2ea3b5bdcdc9f75cbc1
SUDHARSAN SARAVANAN <sudharsansaravanan2623@gmail.com>
2025-05-13 13:03:36 +0530

    Dummy homePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    </div>
+    <main className="min-h-screen bg-sky-900 text-white p-8 sm:p-20 font-sans">
+      <div className="grid grid-rows-[auto_1fr_auto] gap-16 items-center justify-items-center">
+        <section className="text-center max-w-3xl">
+          <h1 className="text-4xl sm:text-6xl font-bold mb-4">
+            Amrita MUN 2025
+          </h1>
+          <div className="mt-6">
+            <Link href="/committee">
+              <button className="bg-white text-[#001f3f] px-6 py-3 rounded-2xl font-semibold hover:bg-gray-200 transition-all">
+                Committees
+              </button>
+            </Link>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
